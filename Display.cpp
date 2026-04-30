53bb56954d318b8ee7552f83d46a1ff73f040bd9
Nil Erdal <nilerdal@gmail.com>
2026-04-30 13:37:40 +0200

    Display functionality implemented

---
+++ Display.cpp
@@ -0,0 +1,29 @@
+#include "Display.h"
+#include <iostream>
+
+void Display::printBoard(Board& board){
+    for(int row = 0; row < 3; row++){
+        for(int col = 0; col < 3; col++){
+            if(col == 2 ) {
+                std::cout << board.getCell(row, col) << std::endl;
+            } else {
+            std::cout << board.getCell(row, col) << ' ' << "|" << ' ';
+            }
+        }
+        if(row < 2){
+            std::cout << std::string(9, '-') << std::endl;
+        }
+    }
+}
+
+void Display::showTurn(Player* player){
+    std::cout << "Turn: " << player->getName() << std::endl;
+}
+
+void Display::showWin(Player* winner){
+    std::cout << "Winner is " << winner->getName() << "!" << std::endl;
+}
+
+void Display::showDraw(bool isDraw){
+    std::cout << "It is a draw!" << std::endl;
+}
---
+++ Display.h
@@ -0,0 +1,11 @@
+#include <string>
+#include "Board.h"
+#include "Player.h"
+
+class Display{
+    public:
+        void printBoard(Board& board);
+        void showTurn(Player* player);
+        void showWin(Player* winner);
+        void showDraw(bool isDraw);
+};
