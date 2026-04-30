a70a46503ab890d8882a153225966eb6c165bcc0
Nil Erdal <nilerdal@gmail.com>
2026-04-30 07:35:24 +0200

    HumanPlayer implemented

---
+++ HumanPlayer.cpp
@@ -0,0 +1,17 @@
+#include "HumanPlayer.h"
+#include "Board.h"
+#include <iostream>
+
+
+HumanPlayer::HumanPlayer(char symbol, std::string name)
+    : Player(symbol, name){}
+
+void HumanPlayer::makeMove(Board& board) {
+    int row, col;
+    std::cout << getName() << "(" << getSymbol() << ")" << "Enter your move (row and column): ";
+    std::cin >> row >> col;
+    while(!board.makeMove(row, col, getSymbol())){
+        std::cout << "Invalid Move!" << "Try Again:";
+        std::cin >> row >> col;
+    }
+}
---
+++ HumanPlayer.h
@@ -0,0 +1,7 @@
+#include "Player.h"
+
+class HumanPlayer : public Player {
+    public:
+    HumanPlayer(char symbol, std::string name);
+    void makeMove(Board& board) override;
+};
