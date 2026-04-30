1955f445084927a3cebaed84280aa93802b357f6
Nil Erdal <nilerdal@gmail.com>
2026-04-30 14:55:23 +0200

    simple game set-up in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <cstdint>
 #include "Game.h"
+#include "HumanPlayer.h"
+#include "AIPlayer.h"
+
 
 int main(int argc, char **argv) {
-    std::cout << "Please feel free to implement your own code in the main.cpp." << std::endl;
+    HumanPlayer p1('X', "Player 1");
+    AIPlayer p2('O', 'X');
+    Game game(&p1, &p2);
+    game.run();
 
     return 0;
 }
