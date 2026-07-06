c36a3318bab280d3326db07e6cee2157d3e62cee
Nil Erdal <nilerdal@gmail.com>
2026-07-06 21:04:33 +0200

    Change human player input logic

--- HumanPlayer.cpp
+++ HumanPlayer.cpp
@@ -7,13 +7,14 @@ HumanPlayer::HumanPlayer(char symbol, std::string name)
     : Player(symbol, name){}
 
 void HumanPlayer::makeMove(Board& board) {
-    int row, col;
-    std::cout << getName() << " (" << getSymbol() << "), " << "Enter row number (0-2): " << std::endl;
-    std::cin >> row;
-    std::cout << "Enter column number (0-2): " << std::endl;
-    std::cin >> col;
+    int row, col, number;
+    std::cout << getName() << " (" << getSymbol() << "), " << "Enter your move (1-9): " << std::endl;
+    std::cin >> number;
+    row = (number - 1) / 3;
+    col = (number - 1) % 3;
     while(!board.makeMove(row, col, getSymbol())){
-        std::cout << "Invalid Move!" << " Try Again:" << std::endl;
-        std::cin >> row >> col;
+        std::cout << "Invalid Move!" << " Try Again." << std::endl;
+        makeMove(board);
+        return;
     }
 }
